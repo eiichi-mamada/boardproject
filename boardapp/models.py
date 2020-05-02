978e6c111bbdb0d2ab36d2cbdb0582bb6aa8dfc5
eiichi-mamada <eastrage@gmail.com>
2020-05-02 12:52:09 +0900

    signup login の実装

--- boardapp/models.py
+++ boardapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class BoardModel(models.Model):
+  title = models.CharField(max_length=100)
+  content = models.TextField()
+  author = models.CharField(max_length=50)
+  image = models.ImageField(upload_to='')
+  good = models.IntegerField()
+  read = models.IntegerField()
+  readtext = models.CharField(max_length=50)
+
+  # class Meta:
+  #       verbose_name = _("BoardModel")
+  #       verbose_name_plural = _("BoardModels")
+
+  #   def __str__(self):
+  #       return self.name
+
+  #   def get_absolute_url(self):
+  #       return reverse("BoardModel_detail", kwargs={"pk": self.pk})
